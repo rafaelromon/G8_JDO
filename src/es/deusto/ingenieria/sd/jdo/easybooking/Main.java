7f3fe58904b02414f558299cee75a04819fa1f46
pablo <pablo@MSI>
2018-10-25 15:53:47 +0100

    Main with variables

--- src/es/deusto/ingenieria/sd/jdo/easybooking/Main.java
+++ src/es/deusto/ingenieria/sd/jdo/easybooking/Main.java
@@ -13,5 +13,19 @@ public class Main {
         Person luis = new Person(2, "Luis");
         Person leyre = new Person(3, "Leyre");
         Person pablo = new Person(4, "Pablo");
+        
+        Airline iberia = new Airline(5, "iberia");
+    	Airline airCanada = new Airline(6,"airCanada");
+    	
+    	Airport madrid = new Airport(7, "barajas");
+    	Airport bilbao = new Airport(8, "laPaloma");
+    	
+    	Reservation R1 = new Reservation(9, "fecha", rafa, F1);
+    	
+    	Plane P1 = new Plane(iberia, 11, madrid, bilbao, 100);
+    	
+    	Flight F1 = new Flight(P1,12,iberia, madrid, bilbao, time,date ,time,date,3,80);
+    	
+    	Reservation R1 = new Reservation(13,date,rafa,F1);
     }
 }
