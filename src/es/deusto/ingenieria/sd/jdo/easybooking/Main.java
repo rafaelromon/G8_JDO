a093de1fbebde18e7cb5019732c39f6209da321c
rafaelromon <rafaelromonsagredo@protonmail.com>
2018-10-25 17:18:55 +0200

    Started Main

--- src/es/deusto/ingenieria/sd/jdo/easybooking/Main.java
+++ src/es/deusto/ingenieria/sd/jdo/easybooking/Main.java
@@ -7,6 +7,11 @@ package es.deusto.ingenieria.sd.jdo.easybooking;
 
 public class Main {
     public static void main(String[] args) {
+        User rafa = new User(1, "Rafael", "rafaelromon@opendeusto.es"
+                , "Google", "Paypal", "Bilbao");
 
+        Person luis = new Person(2, "Luis");
+        Person leyre = new Person(3, "Leyre");
+        Person pablo = new Person(4, "Pablo");
     }
 }
